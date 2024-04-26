dbef058a99c9e7ff6157f0428837be39821b7c71
Rogério Neves dos Santos <103578702+RogerNSantos@users.noreply.github.com>
2024-04-26 17:39:05 -0300

    Add files via upload

---
+++ Udemy_Python_do_Basico_ao_Avancado/Aula01/Aula05_Tipo_Bollean.py
@@ -0,0 +1,11 @@
+# Tipo de dado BOOL (Boolean)
+# Ao questionar algo em um programa, só existem duas respostas possíveis:
+# Sim (TRUE)  ou nÃO (FALSE).
+# Existem vários operadore para "questionar".
+# Dentre eles 0 ==, que é um operador lógico que
+# quationa se um valor é igual a outro
+
+print(10 == 10)
+print(11 == 10)
+print(type(8 == 3))
+print(type(10 == 10))
---
+++ Udemy_Python_do_Basico_ao_Avancado/Aula01/Exercicios/Variaveis_Tipos_De_Dados.py
@@ -0,0 +1,15 @@
+nome = 'Rogério'
+sobre_nome = 'Neves dos Santos'
+idade = 37
+ano_Nascimento = 1986
+altura = 1.70
+maior_De_Idade = idade >= 18
+
+print('Nome:', nome)
+print('Sobre nome:', sobre_nome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_Nascimento)
+print('É maior de Idade?', maior_De_Idade)
+print('Altura em metros:', altura)
+print('=*' * 30)
+print(f'{nome} {sobre_nome}, tem {idade} anos, nasceu em {ano_Nascimento} é maior de idade? {maior_De_Idade}, sua altura é {altura}.')
