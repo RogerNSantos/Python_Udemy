4df944f4abdc6944badee5bd7381efb604822fc7
Rogério Neves dos Santos <103578702+RogerNSantos@users.noreply.github.com>
2024-05-16 17:21:21 -0300

    Add files via upload

---
+++ Udemy_Python_do_Basico_ao_Avancado/Aula01/Exercicios/Calculadora.py
@@ -0,0 +1,46 @@
+""" Calculadora com while """
+while True:
+    numero_1 = input('Digite um número: ')
+    numero_2 = input('Digite outro número: ')
+    operador = input('Digite o operador (+-/*): ')
+
+    numeros_validos = None
+    num_1_float = 0
+    num_2_float = 0
+
+    try:
+        num_1_float = float(numero_1)
+        num_2_float = float(numero_2)
+        numeros_validos = True
+    except:
+        numeros_validos = None
+
+    if numeros_validos is None:
+        print('Um ou ambos os números digitados são inválidos.')
+        continue
+
+    operadores_permitidos = '+-/*'
+
+    if operador not in operadores_permitidos:
+        print('Operador inválido.')
+        continue
+
+    if len(operador) > 1:
+        print('Digite apenas um operador.')
+        continue
+
+    print('Confira o resultado da sua conta.\n')
+    if operador == '+':
+        print(f'A SOMA dos números {num_1_float} + {num_2_float} = ', num_1_float + num_2_float)
+    elif operador == '-':
+        print(f'A SUBTRAÇÃO dos números {num_1_float} - {num_2_float} = ', num_1_float - num_2_float)
+    elif operador == '/':
+        print(f'A DIVISÃO dos número {num_1_float} / {numero_2} = ', num_1_float / num_2_float)
+    elif operador == '*':
+        print(f'A MULTIPLICAÇÃO dos números  {num_1_float} * {num_2_float} = ', num_1_float * num_2_float)
+    
+
+    sair = input('Quer sair? [s]im: ').lower().startswith('s')
+
+    if sair is True:
+        break
